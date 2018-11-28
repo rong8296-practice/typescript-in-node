5be6c9c6d7d7b005acfbf6567265496c7a1d8644
sadhu <rong8296@gmail.com>
2018-11-28 17:49:27 +0800

    test module resolution ok

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,5 @@
-///// <reference types="../my-mod" />
-
-// import osName from 'my-mod'
-// console.log('osName === win32', osName === 'win32')
+import osName from '../my-mod'
+console.log('osName === \'win32\'', osName === 'win32')
 
 import {platform} from 'os'
 console.log('platform() === \'win32\'', platform() === 'win32')
