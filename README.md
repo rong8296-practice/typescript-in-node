c33a3750302de7fe7be35bdc0ff285ad8a8e9bcf
sadhu <rong8296@gmail.com>
2018-12-14 16:21:42 +0800

    classes

--- README.md
+++ README.md
@@ -4,5 +4,16 @@
 
 [rootDirs 说明]
 
+## 修饰符 private 和 pretected
+
+private 只能在声明它的类里使用，[修饰符 private 说明]
+
+protected 能在声明它的类的派生类里使用，[修饰符 protected 说明]
+
+
 
 [rootDirs 说明]: http://www.typescriptlang.org/docs/handbook/module-resolution.html#virtual-directories-with-rootdirs "rootDirs 说明"
+
+[修饰符 private 说明]: http://www.typescriptlang.org/docs/handbook/classes.html#understanding-private "修饰符 private 说明"
+
+[修饰符 protected 说明]: http://www.typescriptlang.org/docs/handbook/classes.html#understanding-protected "修饰符 protected 说明"
--- src/hello.ts
+++ src/hello.ts
@@ -210,3 +210,220 @@ class Button extends Control implements Selectable {
 const btn = new Button(0)
 
 console.log('btn.select(btn.read())', btn.select(btn.read()))
+
+
+;(function(): void {
+
+  class A {
+    // name: string
+    // constructor(name: string) {
+    //   this.name = name
+    // }
+    shout(): void {
+      console.log('shouting')
+    }
+  }
+
+  class AA extends A {
+    read(): string {
+      return 'AA'
+    }
+  }
+
+  const aao: AA = new AA()
+  console.log('aao.read()', aao.read())
+  aao.shout()
+
+})()
+
+;(function(): void {
+
+  class A {
+    private name: string
+    protected age: number
+    constructor(name: string, age: number) {
+      this.name = name
+      this.age = age
+    }
+    readName(): string {
+      return this.name
+    }
+  }
+
+  class AA extends A {
+    constructor(name: string, age: number) {
+      super(name, age)
+    }
+    readAge(): number {
+      return this.age
+    }
+    // readName(): string {
+    //   return this.name // 属性“name”为私有属性，只能在类“A”中访问。
+    // }
+  }
+
+  const ao: A = new A('yaah', 28)
+  const aao: AA = new AA('yooh', 82)
+
+  console.log('ao.readName()', ao.readName())
+
+  // console.log(ao.name) // 属性“name”为私有属性，只能在类“A”中访问。
+  // console.log(aao.name) // 属性“name”为私有属性，只能在类“A”中访问。
+  // console.log(aao.age) // 属性“age”受保护，只能在类“A”及其子类中访问。
+  console.log('aao.readAge()', aao.readAge())
+  // console.log(ao.name)
+
+})()
+
+;(function(): void {
+
+  class A {
+    private _name: string
+    constructor(name: string) {
+      this._name = name + '-ctor'
+    }
+    getOrgName(): string {
+      return this._name
+    }
+    get name(): string {
+      return this._name.split('-')[0]
+    }
+    set name(name: string) {
+      this._name = name + '-by-set'
+    }
+  }
+
+  const ao: A = new A('ao')
+  console.log('ao.getOrgName()', ao.getOrgName())
+  console.log('ao.name', ao.name)
+
+  ao.name = 'new'
+  console.log('ao.getOrgName()', ao.getOrgName())
+  console.log('ao.name', ao.name)
+
+
+})()
+
+;(function(): void {
+
+  class A {
+    private _name: string
+    constructor(name: string) {
+      this._name = name + '-ctor'
+    }
+    getOrgName(): string {
+      return this._name
+    }
+    get name(): string {
+      return this._name.split('-')[0]
+    }
+    // set name(name: string) {
+    //   this._name = name + '-by-set'
+    // }
+  }
+
+  const ao: A = new A('ao')
+  console.log('ao.getOrgName()', ao.getOrgName())
+  console.log('ao.name', ao.name)
+
+  // ao.name = 'new' // Cannot assign to 'name' because it is a read-only property.
+  // console.log('ao.getOrgName()', ao.getOrgName())
+  // console.log('ao.name', ao.name)
+
+
+})()
+
+;(function(): void {
+
+  class A {
+    constructor(
+      readonly name: string,
+      readonly age: number
+    ) {}
+  }
+
+  const ao: A = new A('nao', 28)
+  console.log('ao.name, ao.age', ao.name, ao.age)
+  // ao.name = 'new ao' // Cannot assign to 'name' because it is a read-only property.
+
+
+
+})()
+
+;(function(): void {
+
+  class A {
+    static count: number = 0
+    constructor(
+      readonly name: string,
+      readonly age: number
+    ) {
+      A.count++
+    }
+  }
+
+  const a1: A = new A('a1', 1)
+  const a2: A = new A('a2', 2)
+  const a3: A = new A('a2', 3)
+  console.log('a1, a2, a3', a1, a2, a3)
+  console.log('A.count', A.count)
+  console.log('a2', a2)
+
+})()
+
+;(function(): void {
+
+  abstract class A {
+    constructor(private name: string) {}
+    abstract readName(): string
+    readOrgName(): string {
+      return this.name
+    }
+  }
+
+  class AA extends A {
+    nameAA: string
+    constructor(name: string, nameAA: string) {
+      super(name)
+      this.nameAA = nameAA
+    }
+    readName(): string {
+      // return this.name // 属性“name”为私有属性，只能在类“A”中访问。
+      return 'Cant\'t read name, it\'s private'
+    }
+    readNameAA(): string {
+      return this.nameAA
+    }
+  }
+
+  const aa: AA = new AA('A', 'AA')
+  console.log('aa.readNameAA()', aa.readNameAA())
+  console.log('aa.readName()', aa.readName())
+  console.log('aa.readOrgName()', aa.readOrgName())
+
+})()
+
+;(function(): void {
+  class Greeter {
+    static standardGreeting = "Hello, there";
+    constructor(public greeting?: string) {}
+    greet() {
+      if (this.greeting) {
+        return "Hello, " + this.greeting;
+      }
+      else {
+        return Greeter.standardGreeting;
+      }
+    }
+  }
+
+  let greeter1: Greeter;
+  greeter1 = new Greeter();
+  console.log(greeter1.greet());
+
+  let greeterMaker: typeof Greeter = Greeter;
+  greeterMaker.standardGreeting = "Hey there!";
+
+  let greeter2: Greeter = new greeterMaker();
+  console.log(greeter2.greet());
+})()
