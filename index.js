d4222b052d04b856ec875c7aba9e156ef144e4fb
sadhu <rong8296@gmail.com>
2019-01-22 17:50:16 +0800

    set & map

--- index.js
+++ index.js
@@ -14,3 +14,22 @@ function fn(a, b) {
 const fnf = bound(fn, {it: 'It is this!'})
 
 fnf('one', 'two')
+
+
+const mySet = new Set([1, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5])
+console.log('mySet', mySet)
+console.log(mySet.has(5))
+mySet.add({k: 'k'})
+mySet.add({k: 'k'})
+console.log('mySet', mySet)
+console.log(mySet.has({k: 'k'}))
+const ok = {ok: 'ok'}
+mySet.add(ok)
+mySet.add(ok)
+console.log('mySet', mySet)
+console.log(mySet.has(ok))
+console.log(mySet.size)
+mySet.add(NaN)
+mySet.add(NaN)
+console.log('mySet', mySet)
+console.log(mySet.has(NaN))
